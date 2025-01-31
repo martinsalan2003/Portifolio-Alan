d9cfa7b1aa2a50a48d2b63df36b2285ba9c884d9
AlanMartins00 <alanmartinsferreira59@gmail.com>
2025-01-30 22:30:05 -0300

    upload desktop apresentação

--- portifolioAlan/src/components/sobre.jsx
+++ portifolioAlan/src/components/sobre.jsx
@@ -1,9 +1,29 @@
-import '../styles/components/sobre.sass'
+import '../styles/components/sobre.sass';
+import Alan from '../assets/images/alan-sobre.png';
+import wpp from '../assets/images/whatsapp.svg'
+import git from '../assets/images/github.svg'
+import linkedin from '../assets/images/linkedin.svg'
 
-export default function Sobre(){
-    return(
+export default function Sobre() {
+    return (
         <section>
-            <div><h1>helooo</h1></div>
+            <div className='container-sobre'>
+                <div className="apresentacao">
+                    <h1>Olá, Sou</h1>
+                    <div className='nome'>
+                        <h2>Alan Martins</h2>
+                        <p>Desenvolvedor Front-End / Desenvolvedor Web</p>
+                    </div>
+                    <div className="icones">
+                        <a href="https://api.whatsapp.com/send?phone=11992946537&text=Ola%20Alan,%20estava%20olhando%20um%20pouco%20do%20seu%20trabalho%20no%20Github" target="_blank"><img src={wpp} alt="whatsapp" /></a>
+                        <a href="https://github.com/martinsalan2003"target="_blank"><img src={git} alt="github" /></a>
+                        <a href="https://www.linkedin.com/in/alan-martins-ferreira-5a8a48233/"target="_blank"><img src={linkedin} alt="linkedin" /></a>
+                    </div>
+                </div>
+                <div className="imagem-alan">
+                    <img src={Alan} alt="Alan Martins" />
+                </div>
+            </div>
         </section>
-    )
+    );
 }
